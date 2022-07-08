0f8123d9fc4697544114c304cffbed1bd768125c
Gustavo Kobata <69735196+gustavokobata@users.noreply.github.com>
2022-07-07 22:44:22 -0300

    Delete React directory

--- React/ignite-lab-react/event-platform/src/lib/apollo.ts
+++
@@ -1,6 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4p1o98r1la401z4gc2jhjjy/master',
-    cache: new InMemoryCache()
-})
